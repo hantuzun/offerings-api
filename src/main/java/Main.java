350099dbad9a98a7a09627c04ea2a067f4c97004
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-04-05 23:57:01 +0300

    Return all courses for a given course code as html

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import org.jsoup.Jsoup;
+
+import java.net.URL;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        System.setProperty("javax.net.ssl.trustStore", "src/main/resources/stars.jks");
+
+        get("/:code", (request, response) -> {
+            String url = "https://stars.bilkent.edu.tr/homepage/ajax/plainOfferings.php?COURSE_CODE=" + request.params("code").toUpperCase() + "&SEMESTER=20142";
+            return Jsoup.parse(new URL(url), 10000).html().toString();
+        });
     }
 }
