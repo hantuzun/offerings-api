650ab7cf8e123d25ae58615b1ed410e1b4b07899
Selim <mrsfy@outlook.com>
2017-08-02 18:17:03 -0700

    Stars cert renewed. Offerings year updated to 2017-1 Dependencies updated in pom.xml.(There was a maven repository that does not work) Port default set to 3000. Added Certificate renewing details and contributors to README.

--- src/main/java/CourseCodeTableCrawler.java
+++ src/main/java/CourseCodeTableCrawler.java
@@ -7,7 +7,7 @@ import java.net.URL;
 
 public class CourseCodeTableCrawler {
     public static Elements get(String courseCode) {
-        String url = "https://stars.bilkent.edu.tr/homepage/ajax/plainOfferings.php?SEMESTER=20151&COURSE_CODE=" + courseCode.toUpperCase();
+        String url = "https://stars.bilkent.edu.tr/homepage/ajax/plainOfferings.php?SEMESTER=20171&COURSE_CODE=" + courseCode.toUpperCase();
 
         try {
             Document doc = Jsoup.parse(new URL(url), 10000);
